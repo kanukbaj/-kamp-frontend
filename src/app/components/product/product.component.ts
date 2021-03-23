797a28efca208b52f0b9760be2f25ceeefeac27b
kanukbaj <denizhan.arac@yahoo.com.tr>
2021-03-23 19:49:46 +0300

    ilk comit

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http'
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
 
 @Component({
   selector: 'app-product',
@@ -9,9 +12,16 @@ import { Product } from 'src/app/models/product';
 export class ProductComponent implements OnInit {
   
   products:Product[] = [];
-  constructor() { }
+  apiUrl = "https://localhost:44350/api/products/getall"
+  //productResponseModel : ProductResponseModel={};
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    console.log("Init çalıştı");
+  }
+
+  getProducts(){
+    this.httpClient.get<ProductResponseModel>(this.apiUrl)
   }
 
 }
